b156fcc1235050c9b481d1aa47c784c04499efd3
OmarMeza15 <omarmezajarquin@gmail.com>
2022-12-13 20:26:47 +0100

    added the author for every comment

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -7,7 +7,6 @@ const fileUploader = require("../config/cloudinary.config")
 // Create a new review
 router.post("/reviews", async (req, res, next) => {
     // const { text, imageUrl } = req.body
-
     try {
         const newReview = await Review.create(req.body)
         res.json(newReview)
@@ -21,13 +20,12 @@ router.post("/reviews/upload", fileUploader.single("imageUrl"), (req, res, next)
       next(new Error("No file uploaded!"));
       return;
     }
-    
     res.json({ fileUrl: req.file.path });
-  });
+});
 
 router.get("/reviews", async (req, res, next) => {
     try {
-        const allReviews = await Review.find()
+        const allReviews = await Review.find().populate("author")
         res.json(allReviews)
     } catch (err) {
         console.log(err)
